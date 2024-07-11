cf713b74c151313a985a3220df37c287eea69f9e
Cornelius Motanya <corneliusmot@yahoo.com>
2024-07-11 09:41:49 +0300

    added colors when menu is open or closed

--- app/Components/hamburgerMenu.tsx
+++ app/Components/hamburgerMenu.tsx
@@ -13,25 +13,32 @@ const HamburgerMenu = ({
             aria-label={cn(isOpen ? "Close Menu" : "Open Menu")}
             aria-expanded={isOpen}
             aria-controls="mobile-menu"
-            className="relative z-[1010] flex h-10 w-10 cursor-pointer flex-col items-center justify-center rounded-lg bg-800"
+            className={cn(
+                "relative z-[1010] flex h-12 w-12 cursor-pointer flex-col items-center justify-center rounded-full transition-colors duration-300",
+                isOpen ? "bg-primary" : "bg-800"
+            )}
         >
             <div className="relative h-6 w-6">
                 <span
                     className={cn(
-                        "absolute left-0 h-0.5 w-full transform bg-primary transition duration-300",
-                        isOpen ? "top-3 -rotate-45" : "top-0.5 skew-y-12"
+                        "absolute left-0 h-0.5 w-full transform transition duration-300",
+                        isOpen
+                            ? "top-3 -rotate-45 bg-300"
+                            : "top-0.5 skew-y-12 bg-primary"
                     )}
                 ></span>
                 <span
                     className={cn(
-                        "absolute left-0 top-3 h-0.5 w-full transform bg-primary transition duration-300",
-                        isOpen ? "opacity-0" : "skew-y-12"
+                        "absolute left-0 top-3 h-0.5 w-full transform transition duration-300",
+                        isOpen ? "opacity-0" : "skew-y-12 bg-primary"
                     )}
                 ></span>
                 <span
                     className={cn(
-                        "absolute left-0 h-0.5 w-full transform bg-primary transition duration-300",
-                        isOpen ? "top-3 rotate-45" : "top-[1.35rem] skew-y-12"
+                        "absolute left-0 h-0.5 w-full transform transition duration-300",
+                        isOpen
+                            ? "top-3 rotate-45 bg-300"
+                            : "top-[1.35rem] skew-y-12 bg-primary"
                     )}
                 ></span>
             </div>
