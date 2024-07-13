d8c41b9245394a30f5e9cec3981eaf2ab4337891
Cornelius Motanya <corneliusmot@yahoo.com>
2024-07-13 09:37:13 +0300

    added warehousing section

--- app/service/warehousing/data.tsx
+++ app/service/warehousing/data.tsx
@@ -1,16 +1,34 @@
 export const images = [
     {
+        title: "Global Logistics Expertise",
         src: "https://img.freepik.com/free-photo/cardboard-boxes-conveyor-belt-warehouse_632498-1169.jpg?t=st=1720845270~exp=1720848870~hmac=caaaaa97bfc7dbf4c4d8133a4f8e7b52cf75d235ba003aa1804ad7f287ae29c9&w=740",
         alt: "Cardboard boxes on a conveyor belt in warehouse",
+        content:
+            "Connex International leverages advanced import/export logistics on an international scale. With an extensive multi-modal global transportation network and dedicated import/export logistics specialists, we can provide superior customer care across air and ocean freight forwarding, inland ground distribution, and customs brokerage, with operational support for documentation and regulatory compliance.",
+        buttonText: "Learn More",
     },
     {
+        title: "Comprehensive Ocean Services",
         src: "https://images.pexels.com/photos/236698/pexels-photo-236698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        alt: "Cargo ship in the ocean",
+        content:
+            "Connex's capabilities encompass a broad range of ocean services, including less-than-container loads (LCL), full-container (FCL) loads and dray management. We accommodate bulk cargo — including commodities such as chemicals, raw materials and liquids — as well as vessel chartering and project cargo services.",
+        buttonText: "Discover Services",
     },
     {
+        title: "Expedited Air Services",
         src: "https://images.pexels.com/photos/1427541/pexels-photo-1427541.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        alt: "Airplane cargo",
+        content:
+            "At Connex international, we understand that air services are essential to accelerate the movement of goods and raw materials through the global supply chain. When seeking a partner for time-sensitive transport on import/export logistics, Connex International offers unrivaled air services and expedited transport abilities.",
+        buttonText: "Explore Air Freight",
     },
     {
-        alt: "Close up warehouse view.",
+        title: "Warehouse Solutions",
         src: "https://img.freepik.com/free-photo/close-up-warehouse-view_23-2148923142.jpg?t=st=1720845216~exp=1720848816~hmac=0e53b408c816e5395ac7d8c756c61b17dffa8eb83f0cc0d81017ef1a9cd69f69&w=740",
+        alt: "Close up warehouse view",
+        content:
+            "Connex's capabilities encompass a broad range of ocean services, including less-than-container loads (LCL), full-container (FCL) loads and dray management. We accommodate bulk cargo — including commodities such as chemicals, raw materials and liquids — as well as vessel chartering and project cargo services.",
+        buttonText: "See Warehouse Solutions",
     },
 ];
--- app/service/warehousing/page.tsx
+++ app/service/warehousing/page.tsx
@@ -1,7 +1,72 @@
 import React from "react";
+import Image from "next/image";
+import { images } from "./data";
+import { cn } from "@/app/utils/cn";
+import Footer from "@/app/footer";
 
 const Warehousing = () => {
-    return <div>Warehousing</div>;
+    return (
+        <section className="text-200">
+            <div className="relative h-dvh">
+                <Image
+                    src="https://img.freepik.com/free-photo/interior-large-distribution-warehouse-with-shelves-stacked-with-palettes-goods-ready-market_342744-1481.jpg?t=st=1720847525~exp=1720851125~hmac=aa086d1b760273cc3ff1a57865cfd6b4b333717ded1a90107ac3d1cb347386fc&w=740"
+                    alt=""
+                    fill
+                    placeholder="blur"
+                    blurDataURL="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjAwIiBoZWlnaHQ9IjQwMCI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0iI2YxZjFmMSIvPjwvc3ZnPg=="
+                    className="mx-auto h-[30rem] rounded-sm object-cover object-center"
+                />
+                <div className="absolute inset-0 h-full backdrop-blur-sm"></div>
+                <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-balance rounded-md bg-850 p-6 text-center text-300 md:whitespace-nowrap md:text-start md:text-5xl">
+                    Comprehensive Warehousing Solution
+                </h1>
+            </div>
+
+            <div>
+                {images.map((item, index) => (
+                    <div
+                        key={index}
+                        className={cn(
+                            "flex gap-8 p-2 md:p-5",
+                            index % 2 === 0 ? "bg-800 text-400" : "bg-950"
+                        )}
+                    >
+                        <div
+                            className={cn(
+                                "mx-auto flex w-full flex-row gap-2 py-3 md:w-[80%] md:gap-8",
+                                index % 2 === 0
+                                    ? "flex-col md:flex-row"
+                                    : "flex-col md:flex-row-reverse"
+                            )}
+                        >
+                            <div className="mb-3 w-full md:w-[50%]">
+                                <Image
+                                    src={item.src}
+                                    alt={item.alt}
+                                    width={400}
+                                    height={400}
+                                    className="w-full rounded-md object-cover object-center"
+                                />
+                            </div>
+                            <div className="flex w-full flex-col justify-around space-y-3 md:w-[50%]">
+                                <h3 className="text-2xl font-bold uppercase text-primary">
+                                    {item.title}
+                                </h3>
+                                <p className="w-full text-balance text-lg">
+                                    {item.content}
+                                </p>
+                                <button className="flex w-fit justify-start rounded-full bg-primary px-4 py-2 font-roboto-bold font-bold text-black">
+                                    {item.buttonText}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <Footer />
+        </section>
+    );
 };
 
 export default Warehousing;
