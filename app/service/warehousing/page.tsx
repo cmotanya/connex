cddc8105a0b066d86b5257880b7eb5ececfba30f
Cornelius Motanya <corneliusmot@yahoo.com>
2024-07-14 12:13:17 +0300

    added framer-motion animation for view once whileInUse

--- app/service/warehousing/page.tsx
+++ app/service/warehousing/page.tsx
@@ -78,8 +78,16 @@ const Warehousing = () => {
                         >
                             <motion.div
                                 className="w-full md:w-[50%]"
+                                initial={{ scale: 0.5, opacity: 0 }}
+                                whileInView={{ scale: 1, opacity: 1 }}
                                 whileHover={{ scale: 1.05 }}
-                                transition={{ type: "spring", stiffness: 300 }}
+                                transition={{
+                                    type: "spring",
+                                    stiffness: 300,
+                                    delay: 0.2,
+                                    duration: 0.5,
+                                }}
+                                viewport={{ once: true, amount: 0.5 }}
                             >
                                 <Image
                                     src={item.src}
